09885413879c188adc9e6933beebe775418365ad
Steve Phelps <sphelps@sphelps.net>
2023-04-25 14:09:26 +0100

    prompt makes use payoff parameters

--- dilemma.py
+++ dilemma.py
@@ -37,11 +37,11 @@ You will be making your choices with an investment partner who is another person
 In a minute I will take you there yo meet your investment partner.  
 You and your partner will be choosing between two investment projects and you will do so {n} times. 
 The object is to earn as much money as possible.  
-If you and your partner choose 'project green' you will both earn $5.00.  
-If you choose "project blue" but your partner chooses "project green" then you will receive $7.00 
-and your partner will receive $3.00.  If you both choose project blue you will each receive nothing.  
-If your partner chooses "project blue" and you choose "project green" then you will receive $3.00 and your 
-partner will receive $7.00.  I will tell you what your partner chooses in subsequent prompts, 
+If you and your partner choose 'project green' you will both earn ${R}.00.  
+If you choose "project blue" but your partner chooses "project green" then you will receive ${T}.00 
+and your partner will receive ${S}.00.  If you both choose project blue you will each receive ${S}.00
+If your partner chooses "project blue" and you choose "project green" then you will receive ${S}.00 and your 
+partner will receive ${T}.00.  I will tell you what your partner chooses in subsequent prompts, 
 but you will make your choice ahead of your partner telling me your choice. 
 Here is your investment partner.
 What is your first choice?
