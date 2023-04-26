814177e701bcca9d873e720e9524391917fc0a4a
Steve Phelps <sphelps@sphelps.net>
2023-04-26 20:58:01 +0100

    refactored in functional style

--- gpt.py
+++ gpt.py
@@ -19,31 +19,31 @@ Completion = Dict[str, str]
 Conversation = Iterable[Completion]
 
 
-def generate_completions(messages: Conversation, n: int = 1) -> Iterable[Completion]:
+def generate_completions(
+    messages: Conversation, n: int = 1, retries=0
+) -> Iterable[Completion]:
     logger.debug("messages = %s", messages)
-    retries = 0
-    while retries < GPT_MAX_RETRIES:
-        try:
-            result = openai.ChatCompletion.create(
-                model=GPT_ENGINE,
-                messages=messages,
-                max_tokens=GPT_MAX_TOKENS,
-                n=n,
-                temperature=GPT_TEMPERATURE,
-            )
-            logger.debug("response = %s", result)
-            for choice in result.choices:
-                yield choice.message
-            break
-        except (RateLimitError, APIError) as err:
-            if isinstance(err, APIError) and not (err.http_status in [524, 502]):
-                raise
-            logger.warning("Error returned from openai API: %s", err)
-            logger.debug("retries = %d", retries)
-            retries += 1
-            if retries < GPT_MAX_RETRIES:
-                logger.info("Retrying... ")
-                time.sleep(20 + 2**retries)
-            else:
-                logger.error("Maximum retries reached, aborting.")
-                raise
+    try:
+        result = openai.ChatCompletion.create(
+            model=GPT_ENGINE,
+            messages=messages,
+            max_tokens=GPT_MAX_TOKENS,
+            n=n,
+            temperature=GPT_TEMPERATURE,
+        )
+        logger.debug("response = %s", result)
+        for choice in result.choices:
+            yield choice.message
+    except (RateLimitError, APIError) as err:
+        if isinstance(err, APIError) and not (err.http_status in [524, 502]):
+            raise
+        logger.warning("Error returned from openai API: %s", err)
+        logger.debug("retries = %d", retries)
+        if retries < GPT_MAX_RETRIES:
+            logger.info("Retrying... ")
+            time.sleep(20 + 2**retries)
+            for completion in generate_completions(messages, n, retries + 1):
+                yield completion
+        else:
+            logger.error("Maximum retries reached, aborting.")
+            raise
