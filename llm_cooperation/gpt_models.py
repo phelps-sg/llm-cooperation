7a0927b30a96d8628a11df90d0b768813d273c19
Steve Phelps <phelps.sg@gmail.com>
2023-06-19 11:08:56 +0100

    add script to output gpt models

---
+++ llm_cooperation/gpt_models.py
@@ -0,0 +1,10 @@
+""" Script to output all available GPT models """
+import os
+
+import openai
+
+if __name__ == "__main__":
+    openai.api_key = os.environ["OPENAI_API_KEY"]
+    models = openai.Model.list()
+    for model_id in [m["id"] for m in models.data if "gpt" in m["id"]]:
+        print(model_id)
