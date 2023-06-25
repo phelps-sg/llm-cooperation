da60afc787f1bc5ea05af063a01e7615f27c125e
Steve Phelps <phelps.sg@gmail.com>
2023-06-25 11:06:41 +0100

    configurable grid for model setup

--- llm_cooperation/main.py
+++ llm_cooperation/main.py
@@ -1,9 +1,10 @@
+import itertools
 import logging
-from typing import Dict
+from typing import Dict, Iterable, List
 
 import openai_pygenerator as oai
 
-from llm_cooperation import Experiment
+from llm_cooperation import Experiment, ModelSetup
 from llm_cooperation.experiments import (
     dictator,
     dilemma,
@@ -24,17 +25,53 @@ experiments: Dict[str, Experiment] = {
     "dictator": dictator.run_experiment_dictator,
 }
 
+ConfigValue = float | str
+Grid = Dict[str, List[ConfigValue]]
+Settings = Dict[str, ConfigValue]
+
+DEFAULT_GRID: Grid = {"temperature": [oai.GPT_TEMPERATURE], "model": [oai.GPT_MODEL]}
+
+
+def get_grid() -> Grid:
+    try:
+        config = __import__("llm_config")
+        return config.grid
+    except ModuleNotFoundError:
+        logger.info("Could not find llm_config.py in PYTHONPATH")
+    except KeyError:
+        logger.info("Could not find grid in llm_config.py")
+    logger.info("Using default grid")
+    return DEFAULT_GRID
+
+
+def setup_from_settings(settings: Settings) -> ModelSetup:
+    return ModelSetup(
+        model=str(settings["model"]), temperature=float(settings["temperature"])
+    )
+
+
+def settings_generator(grid: Grid) -> Iterable[Settings]:
+    keys = list(grid.keys())
+    value_combinations = itertools.product(*grid.values())
+    for values in value_combinations:
+        settings: Dict[str, ConfigValue] = dict()
+        for i, value in enumerate(values):
+            settings[keys[i]] = value
+        yield settings
+
 
 def run_all() -> None:
-    for name, experiment in experiments.items():
-        logger.info(
-            "Running experiment %s with model %s and temperature %.02f ...",
-            name,
-            oai.GPT_MODEL,
-            oai.GPT_TEMPERATURE,
-        )
-        run_and_record_experiment(name, experiment)
-        logger.info("Experiment %s completed.", name)
+    for settings in settings_generator(get_grid()):
+        setup = setup_from_settings(settings)
+        for name, experiment in experiments.items():
+            logger.info(
+                "Running experiment %s with model %s and temperature %.02f ...",
+                name,
+                setup.model,
+                setup.temperature,
+            )
+            run_and_record_experiment(name, experiment, setup)
+            logger.info("Experiment %s completed.", name)
 
 
 if __name__ == "__main__":
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,13 +1,46 @@
-from unittest.mock import call
+import pytest
 
-from llm_cooperation.main import experiments, run_all
+from llm_cooperation import ModelSetup
+from llm_cooperation.main import (
+    Grid,
+    Settings,
+    experiments,
+    run_all,
+    settings_generator,
+    setup_from_settings,
+)
 
 
-def test_run_all(mocker):
+@pytest.mark.parametrize(
+    "settings, expected_result",
+    [
+        (
+            {"model": "gpt-turbo-3.5", "temperature": 0.5},
+            ModelSetup(model="gpt-turbo-3.5", temperature=0.5),
+        )
+    ],
+)
+def test_setup_from_settings(settings: Settings, expected_result: ModelSetup):
+    result = setup_from_settings(settings)
+    assert result == expected_result
+
+
+def test_settings_generator(grid):
+    result = list(settings_generator(grid))
+    for setting in result:
+        for key, value in setting.items():
+            assert value in grid[key]
+
+
+def test_run_all(mocker, grid):
     run_and_record = mocker.patch(
         "llm_cooperation.main.run_and_record_experiment", return_value=None
     )
+    mocker.patch("llm_cooperation.main.get_grid", return_value=grid)
     run_all()
-    run_and_record.assert_has_calls(
-        [call(experiment, runnable) for experiment, runnable in experiments.items()]
-    )
+    assert run_and_record.call_count == 6 * len(list(experiments.items()))
+
+
+@pytest.fixture
+def grid() -> Grid:
+    return {"temperature": [0.2, 0.3], "model": ["x", "y", "z"]}
