4a7b9fc1602a33f9932891b566830f57f09dbb0f
Steve Phelps <phelps.sg@gmail.com>
2023-11-05 13:51:46 +0000

    fixed bug in tit for tat

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,13 +7,15 @@ from openai_pygenerator import Completion
 @pytest.fixture
 def conversation() -> List[Completion]:
     return [
-        {"user": "scenario prompt"},
+        {"user": "scenario prompt.  What is your choice in the first round?"},
         {"role": "assistant", "content": "project green"},
-        {"role": "user", "content": "project blue"},
+        {"role": "user", "content": "Your partner chose project blue"},
         {"role": "assistant", "content": "project blue"},
-        {"role": "user", "content": "project green"},
+        {"role": "user", "content": "Your partner chose project green"},
         {"role": "assistant", "content": "project blue"},
-        {"role": "user", "content": "project blue"},
+        {"role": "user", "content": "Your partner chose project blue"},
         {"role": "assistant", "content": "project blue"},
+        {"role": "user", "content": "Your partner chose project blue"},
+        {"role": "assistant", "content": "project green"},
         {"role": "user", "content": "project green"},
     ]
